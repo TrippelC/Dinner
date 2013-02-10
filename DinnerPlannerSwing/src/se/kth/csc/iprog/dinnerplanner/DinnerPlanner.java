46cfe8a42c83b6364ee25cfeebb9a814b282476c
trippelc <chjun_chi@hotmail.com>
2013-02-10 17:24:21 +0100

    Update model

--- DinnerPlannerSwing/src/se/kth/csc/iprog/dinnerplanner/DinnerPlanner.java
+++ DinnerPlannerSwing/src/se/kth/csc/iprog/dinnerplanner/DinnerPlanner.java
@@ -34,7 +34,7 @@ public class DinnerPlanner extends JFrame {
 		
 		//test no more :D
 		// Foo
-		//bar
+		// bar
 		dinnerPlanner.setTitle("Dinner Planner");
 			
 		//Getting some dishes for setup
--- DinnerPlannerSwing/src/se/kth/csc/iprog/dinnerplanner/model/DinnerModel.java
+++ DinnerPlannerSwing/src/se/kth/csc/iprog/dinnerplanner/model/DinnerModel.java
@@ -41,6 +41,14 @@ public class DinnerModel extends Observable implements IDinnerModel {
 		dish1.addIngredient(dish1ing5);
 		dishes.add(dish1);
 		
+		Dish dish6 = new Dish("toast",Dish.STARTER,"toast.jpg","In a large mixing bowl, beat the eggs. Add the milk, brown sugar and nutmeg; stir well to combine. Soak bread slices in the egg mixture until saturated. Heat a lightly oiled griddle or frying pan over medium high heat. Brown slices on both sides, sprinkle with cinnamon and serve hot.");
+		dish6.addIngredient(dish1ing1);
+		dish6.addIngredient(dish1ing2);
+		dish6.addIngredient(dish1ing3);
+		dish6.addIngredient(dish1ing4);
+		dish6.addIngredient(dish1ing5);
+		dishes.add(dish6);
+		
 		Dish dish2 = new Dish("Meat balls",Dish.MAIN,"meatballs.jpg","Preheat an oven to 400 degrees F (200 degrees C). Place the beef into a mixing bowl, and season with salt, onion, garlic salt, Italian seasoning, oregano, red pepper flakes, hot pepper sauce, and Worcestershire sauce; mix well. Add the milk, Parmesan cheese, and bread crumbs. Mix until evenly blended, then form into 1 1/2-inch meatballs, and place onto a baking sheet. Bake in the preheated oven until no longer pink in the center, 20 to 25 minutes.");
 		Ingredient dish2ing1 = new Ingredient("extra lean ground beef",115,"g",20);
 		Ingredient dish2ing2 = new Ingredient("sea salt",0.7,"g",3);
@@ -65,6 +73,21 @@ public class DinnerModel extends Observable implements IDinnerModel {
 		dish2.addIngredient(dish2ing10);
 		dish2.addIngredient(dish2ing11);
 		dishes.add(dish2);
+		
+		Dish dish7 = new Dish("balls",Dish.MAIN,"meatballs.jpg","Preheat an oven to 400 degrees F (200 degrees C). Place the beef into a mixing bowl, and season with salt, onion, garlic salt, Italian seasoning, oregano, red pepper flakes, hot pepper sauce, and Worcestershire sauce; mix well. Add the milk, Parmesan cheese, and bread crumbs. Mix until evenly blended, then form into 1 1/2-inch meatballs, and place onto a baking sheet. Bake in the preheated oven until no longer pink in the center, 20 to 25 minutes.");
+		dish7.addIngredient(dish2ing1);
+		dish7.addIngredient(dish2ing2);
+		dish7.addIngredient(dish2ing3);
+		dish7.addIngredient(dish2ing4);
+		dish7.addIngredient(dish2ing5);
+		dish7.addIngredient(dish2ing6);
+		dish7.addIngredient(dish2ing7);
+		dish7.addIngredient(dish2ing8);
+		dish7.addIngredient(dish2ing9);
+		dish7.addIngredient(dish2ing10);
+		dish7.addIngredient(dish2ing11);
+		dishes.add(dish7);
+		
 		
 		Dish dish3 = new Dish("Ice Cream",Dish.DESERT,"icecream.jpg","In a large mixing bowl, beat the eggs. Add the milk, brown sugar and nutmeg; stir well to combine. Soak bread slices in the egg mixture until saturated. Heat a lightly oiled griddle or frying pan over medium high heat. Brown slices on both sides, sprinkle with cinnamon and serve hot.");
 		Ingredient dish3ing1 = new Ingredient("eggs",0.5,"",1);
